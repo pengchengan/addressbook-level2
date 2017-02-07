d5851e4cc499b176fd0c6aa7a733f3cf632b67ae
DESKTOP-49OHI99\user <pengchengan@u.nus.edu>
2017-02-08 01:46:56 +0800

    Create PersonNameTest class and test nothing

---
+++ test/java/seedu/addressbook/util/PersonNameTest.java
@@ -0,0 +1,20 @@
+package seedu.addressbook.util;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+import seedu.addressbook.data.AddressBook;
+import seedu.addressbook.data.exception.IllegalValueException;
+import seedu.addressbook.data.person.Name;
+
+public class PersonNameTest {
+	@Test
+	public void TestSomething(){
+		assertFalse(false);
+	}
+}
