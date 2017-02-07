ca721d689b70f827a43edfe35bb92d6a777fa671
DESKTOP-49OHI99\user <pengchengan@u.nus.edu>
2017-02-08 02:00:05 +0800

    Add isSimilar function and testIdentical

--- test/java/seedu/addressbook/util/PersonNameTest.java
+++ test/java/seedu/addressbook/util/PersonNameTest.java
@@ -12,9 +12,19 @@ import seedu.addressbook.data.AddressBook;
 import seedu.addressbook.data.exception.IllegalValueException;
 import seedu.addressbook.data.person.Name;
 
+/**
+ * @author user
+ * This class is used to test isSimilar function in class Name
+ */
 public class PersonNameTest {
 	@Test
-	public void TestSomething(){
-		assertFalse(false);
+	public void TestIdentical(){
+		try {
+			assertFalse(new Name("aaa").isSimilar(new Name("aaa")));
+			assertFalse(new Name("a a").isSimilar(new Name("a a")));
+			assertFalse(new Name("ab ab ab").isSimilar(new Name("ab ab ab")));
+		} catch(IllegalValueException e) {
+			System.out.println(e);
+		}
 	}
 }
